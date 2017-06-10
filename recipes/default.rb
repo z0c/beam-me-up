0a415083928ec764f2b24ac505f497abe254b377
Your Name <you@example.com>
2017-06-10 21:25:41 +0000

    More packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,4 +5,9 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 include_recipe 'beam-me-up::desktop'
+package 'curl'
+package 'git'
+package 'screen'
 package 'terminator'
+package 'tree'
+package 'vim'
