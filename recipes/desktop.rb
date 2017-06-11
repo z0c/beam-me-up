99f4f250973b6300302229d91b22ca852ef9e671
Your Name <you@example.com>
2017-06-11 22:14:32 +0000

    Tidy up

--- recipes/desktop.rb
+++ recipes/desktop.rb
@@ -5,12 +5,8 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 bash 'set_resolution' do
-  code <<-EOH
-    pdisplay='#{node['beam-me-up']['display']}'
-    if xrandr 2>/dev/null | grep -q "#{node['beam-me-up']['display']}"; then
-      xrandr -s $pdisplay
-    fi
-  EOH
+  code "xrandr -s #{node['beam-me-up']['display']}"
+  only_if "xrandr 2>/dev/null | grep -q \"#{node['beam-me-up']['display']}\""
   # Second grep is NOT working in here
   not_if "xrandr | grep -q \"#{node['beam-me-up']['display']}\" | grep \\*"
 end
