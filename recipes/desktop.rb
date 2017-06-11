7731e9a744eb687b577fc982542fc3a5bdabca01
Your Name <you@example.com>
2017-06-11 21:46:23 +0000

    Fixed rubocop defects

--- recipes/desktop.rb
+++ recipes/desktop.rb
@@ -4,13 +4,12 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-bash "set_resolution" do
+bash 'set_resolution' do
   code <<-EOH
     pdisplay='#{node.default['beam-me-up']['display']}'
     if xrandr 2>/dev/null | grep -q "#{node.default['beam-me-up']['display']}"; then
       xrandr -s $pdisplay
     fi
   EOH
-  not_if "xrandr 2>/dev/null | grep -q \"#{node.default['beam-me-up']['display']}\" | grep \*"
+  not_if "xrandr 2>/dev/null | grep -q \"#{node.default['beam-me-up']['display']}\" | grep -c \*"
 end
-
