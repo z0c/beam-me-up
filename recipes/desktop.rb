a24d9209b453788684282b7ab646926ad3396970
Your Name <you@example.com>
2017-06-11 22:08:08 +0000

    Cleanup

--- recipes/desktop.rb
+++ recipes/desktop.rb
@@ -6,10 +6,11 @@
 
 bash 'set_resolution' do
   code <<-EOH
-    pdisplay='#{node.default['beam-me-up']['display']}'
-    if xrandr 2>/dev/null | grep -q "#{node.default['beam-me-up']['display']}"; then
+    pdisplay='#{node['beam-me-up']['display']}'
+    if xrandr 2>/dev/null | grep -q "#{node['beam-me-up']['display']}"; then
       xrandr -s $pdisplay
     fi
   EOH
-  not_if "xrandr 2>/dev/null | grep -q \"#{node.default['beam-me-up']['display']}\" | grep -c \*"
+  # Second grep is NOT working in here
+  not_if "xrandr | grep -q \"#{node['beam-me-up']['display']}\" | grep \\*"
 end
