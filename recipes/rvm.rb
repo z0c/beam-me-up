899a2c815e302184c11bc47351ede1bb584fb951
Your Name <you@example.com>
2017-06-12 21:15:09 +0000

    Fixed RVM

---
+++ recipes/rvm.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: beam-me-up
+# Recipe:: rvm
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['rvm']['user_installs'] = [{ 
+  'user'	 => 'lubuntu',
+  'default_ruby' => 'ruby-2.5.5',
+  'user_global_gems'  => [
+    {'name': 'bundler'}
+  ]
+}]
+
+include_recipe 'rvm::user'
