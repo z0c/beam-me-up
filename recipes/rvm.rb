c0dca48903e165a90b3d2034e21721cc60c66aa7
Your Name <you@example.com>
2017-06-13 20:21:11 +0000

    Moved RVM attributes to role

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -4,13 +4,4 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-node.default['rvm']['user_installs'] = [{ 
-  'user'	 => 'lubuntu',
-  'default_ruby' => 'ruby-2.2.5',
-  'rubies'       => ['2.2.5'],
-  'user_global_gems'  => [
-    {'name': 'bundler'}
-  ]
-}]
-
 include_recipe 'rvm::user'
