42638ff6ae8b3a93f3511a7ffb23a2cf5cd5a9c3
Your Name <you@example.com>
2017-06-16 20:25:27 +0000

    Added vim ruby

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures beam-me-up'
 long_description 'Installs/Configures beam-me-up'
-version '0.2.0'
+version '0.3.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/z0c/beam-me-up/issues'
 source_url 'https://github.com/z0c/beam-me-up'
--- recipes/vim.rb
+++ recipes/vim.rb
@@ -9,3 +9,13 @@ package 'vim'
 pathogen_base 'install pathogen!' do
   users [node['beam-me-up']['user']]
 end
+
+pathogen_plugin 'vim-airline' do
+  github_org 'vim-airline'
+  users [node['beam-me-up']['user']]
+end
+
+pathogen_plugin 'vim-fugitive' do
+  github_org 'tpope'
+  users [node['beam-me-up']['user']]
+end
