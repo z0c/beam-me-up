f9a3be8f012b1a14076374161b91f8e13505a884
Your Name <you@example.com>
2017-06-30 21:27:55 +0000

    Added desktop settings

--- recipes/desktop.rb
+++ recipes/desktop.rb
@@ -9,3 +9,10 @@ bash 'set_resolution' do
   only_if "xrandr 2>/dev/null | grep -q \"#{node['beam-me-up']['display']}\""
   not_if "xrandr | grep \"#{node['beam-me-up']['display']}\" | grep '*'"
 end
+
+template "/home/#{node['beam-me-up']['user']}/.config/pcmanfm/lubuntu/desktop-items-0.conf" do
+  source 'desktop-items-0.conf.erb'
+  owner node['beam-me-up']['user']
+  group node['beam-me-up']['user']
+  mode '0664'
+end
